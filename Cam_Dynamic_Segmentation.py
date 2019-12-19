39ad10833d118dfa509876d9e669f033db2ff30a
Lucas Ikuhara <lri2911@gmail.com>
2019-12-19 02:38:58 -0300

    color picker improvements

--- Cam_Dynamic_Segmentation.py
+++ Cam_Dynamic_Segmentation.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import time as t
 import random as rnd
-import math
 
 
 def randomWalkSampler(picture, n=30):
@@ -15,33 +14,45 @@ def randomWalkSampler(picture, n=30):
     for elem in samples:
         for value in elem:
             sum += value
+    
     return sum/(len(samples)*3)
+
+def colorNormalizer(color):
+    for i in range(3):
+        if color[i]>255:
+            color[i] = 255
+        if color[i]<0:
+            color[i] = 0
+    return color
 
 def colorBoundariesGenerator(color, threshold=15):
     aux = np.array((threshold, threshold, threshold))
     bright = color + aux
     dark = color - aux
-
-    for i in range(3):
-        if dark[i]>255:
-            dark[i] = 255
-        if dark[i]<0:
-            dark[i] = 0
-
-    for i in range(3):
-        if bright[i]>255:
-            bright[i] = 255
-        if bright[i]<0:
-            bright[i] = 0
+    
+    bright = colorNormalizer(bright)
+    dark = colorNormalizer(dark)
 
     return dark, bright
 
-def colorScalling(color, originalBright, actualBright):
-    pass
+def colorScallingAdditive(color, originalBright, currentBright):
+    delta = currentBright - originalBright 
+    color = color + np.array([delta, delta, delta])
+    color = colorNormalizer(color)
+    
+    return color
 
-def colorSelect(cap):
+def colorScallingPercentual(color, originalBright, currentBright):
+    k = originalBright/currentBright
+    color = np.array([color[0]*k, color[1]*k, color[2]*k])
+    color = colorNormalizer(color)
+
+    return color
+
+def colorSelect(cap, brightnessSamples=30):
     dim = cap.read()[1].shape
-    centerPoint = (int(dim[0]/2), int(dim[0]/2))
+    centerPoint = (int(dim[1]/2), int(dim[0]/2))
+    
     point1 = (centerPoint[0]-20, centerPoint[1]-20)
     point2 = (centerPoint[0]-20, centerPoint[1]+20)
     point3 = (centerPoint[0]+20, centerPoint[1]+20)
@@ -51,10 +62,11 @@ def colorSelect(cap):
     while(True):
         ret, frame = cap.read()
         #Draw Square
-        cv2.line(frame, point1, point2, red )
-        cv2.line(frame, point2, point3, red )
-        cv2.line(frame, point3, point4, red )
-        cv2.line(frame, point4, point1, red )
+        cv2.line(frame, point1, point2, red, 2 )
+        cv2.line(frame, point2, point3, red, 2 ) 
+        cv2.line(frame, point3, point4, red, 2 )
+        cv2.line(frame, point4, point1, red, 2 )
+        
         cv2.imshow("Choose a color by pressing space", frame)
         
         #Choose a color by pressing space.
@@ -66,8 +78,8 @@ def colorSelect(cap):
 
     #Store All pixels within the red border
     pixels = []
-    for i in range(point1[0], point3[0]):
-        for j in range(point1[1], point3[1]):
+    for i in range(point1[1], point3[1]):
+        for j in range(point1[0], point3[0]):
             pixels.append(frame[i][j])
 
     #Get and return the mean of each color value
@@ -78,14 +90,21 @@ def colorSelect(cap):
         R_sum += pixels[i][2]
     mean = lambda val: int(val/len(pixels))
 
-    return np.array([mean(B_sum), mean(G_sum), mean(R_sum)]) 
+    color = np.array([mean(B_sum), mean(G_sum), mean(R_sum)])
+    
+    #Show the chosen color
+    display = np.zeros((1, 1, 3), np.uint8)
+    display[0][0] = color
+    display = cv2.resize(display, (100, 100))
+    cv2.imshow("Chosen color:", display)
+
+    return color, randomWalkSampler(frame, brightnessSamples) 
 
 
 cap = cv2.VideoCapture(0)
-#TargetColor = (int(input("R: ")), int(input("G: ")), int(input("B: ")))
 walkDistance = int(input("Brightness Samples: "))
 threshold = int(input("Color Threshold: "))
-TargetColor = colorSelect(cap)
+TargetColor, TargetColorBrightness = colorSelect(cap)
 print("Selected color: ", TargetColor)
 FPS = 0
 lastSec = t.time()
@@ -94,16 +113,34 @@ lastSec = t.time()
 while(True):
     #ret is a boolean which acts a flag regarding whether read() was successful
     ret, frame = cap.read()
+    meanBrightness = randomWalkSampler(frame, walkDistance)
+    cv2.imshow("Input Video", frame)
+
+    #regular segmentation
     targetDark, targetBright = colorBoundariesGenerator(TargetColor, threshold)
     mask = cv2.inRange(frame, targetDark, targetBright)
-    cv2.imshow("Input Video", frame)
-    cv2.imshow("Segmented Video", mask)
-    meanBrightness = randomWalkSampler(frame, 100)
+    cv2.imshow("Normal segmentation", mask)
+
+    #Gauss filter
+    frame = cv2.filter2D(frame, -1, (np.array([(1, 2, 1), (2, 4, 2), (1, 2, 1)])/16) )
+
+    #addition-based correction
+    colorScallingAdditive(TargetColor, TargetColorBrightness, meanBrightness)
+    targetDark, targetBright = colorBoundariesGenerator(TargetColor, threshold)
+    mask = cv2.inRange(frame, targetDark, targetBright)
+    cv2.imshow("Segmented with additive correction", mask)
     
+    #percentage-based correction
+    colorScallingPercentual(TargetColor, TargetColorBrightness, meanBrightness)
+    targetDark, targetBright = colorBoundariesGenerator(TargetColor, threshold)
+    mask = cv2.inRange(frame, targetDark, targetBright)
+    cv2.imshow("Segmented with percentual correction", mask)
+
     FPS += 1
     if t.time() > lastSec+1:
         lastSec = t.time()
         print("FPS: ",FPS)
+        print("Mean Brightness: ",meanBrightness)
         FPS=0
 
     #makes it so you can close the program by pressing ESC.
