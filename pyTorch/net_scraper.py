dea3ca0ed6593b7d81e417dd47112492a83c3f8b
LucasIkuhara <lri2911@gmail.com>
2020-01-08 10:55:06 -0300

    Added 'monitoring functionality' to the net_scraper

--- pyTorch/net_scraper.py
+++ pyTorch/net_scraper.py
@@ -2,16 +2,43 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-driver = webdriver.Chrome(r"C:\Users\Lucas Ikuhara\Pictures\testzone\pyTorch\chromedriver.exe")
-driver.get('https://www.probuilds.net/')
+#driver = webdriver.Chrome(r"C:\Users\Lucas Ikuhara\Pictures\testzone\pyTorch\chromedriver.exe")
+#driver.get('https://www.probuilds.net/')
 
-for i in range(20):
-    load_more = driver.find_element_by_id("moreMatchesButton")
-    load_more.click()
+#for i in range(20):
+#    load_more = driver.find_element_by_id("moreMatchesButton")
+ #   load_more.click()
+iterationCounter = 0
+interval = 1*60*60
 
-sourcePage = requests.get("https://www.probuilds.net/").text
-soup = BeautifulSoup(sourcePage, 'lxml')
+while True:
+    sourcePage = requests.get("https://www.probuilds.net/livefeed").text
+    soup = BeautifulSoup(sourcePage, 'lxml')
 
-LiveFeed = soup.find('div', id="game-feed") #class_="game-feed pro-history pos-rel") #"game-feed")
-links = soup.find_all("a")
-print(links)
+    #Get all matches from 'Live Feed' page
+    LiveFeed = soup.find('div', id="game-feed") #class_="game-feed pro-history pos-rel") #"game-feed")
+    links = soup.find_all("a")
+
+    #List links we already have
+    linksFile = open("links.txt", "r")
+    linksInFile = linksFile.readlines()
+    linksFile.close()
+
+    #Sort new matches
+    linksFile = open("links.txt", "a")
+    newLinks = []
+    for address in links:
+        if address not in linksInFile:
+            linksInFile.append(address)
+            newLinks.append(address)
+
+    #Add new matches to the txt file
+    for address in newLinks:
+        linksFile.write(address+"\n")
+    linksFile.flush()
+    linksFile.close()
+
+    #Iteration control
+    iterationCounter +=1
+    print("Elapsed runs: ", iterationCounter)
+    print("Feel free to close this at any given time. Current iterations are set tp happen every {} hour(s)".format(interval/3600))
