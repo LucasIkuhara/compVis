2ee162b55dd67c19735e2a96d76ca70a9f37edd4
Lucas Ikuhara <lri2911@gmail.com>
2020-01-15 22:46:26 -0300

    Finished net_scraper

--- pyTorch/net_scraper.py
+++ pyTorch/net_scraper.py
@@ -1,44 +1,59 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import requests
 
-#driver = webdriver.Chrome(r"C:\Users\Lucas Ikuhara\Pictures\testzone\pyTorch\chromedriver.exe")
-#driver.get('https://www.probuilds.net/')
+n = int(int(input("How many games should be fetched (Approx): "))/10)
+maxFailedAttempts = int(input("Fail tolerance: "))
 
-#for i in range(20):
-#    load_more = driver.find_element_by_id("moreMatchesButton")
- #   load_more.click()
-iterationCounter = 0
-interval = 1*60*60
+#Driver declaration
+driver = webdriver.Chrome(r"C:\Users\Lucas Ikuhara\Pictures\testzone\pyTorch\chromedriver.exe")
+driver.get('https://www.probuilds.net/')
 
-while True:
-    sourcePage = requests.get("https://www.probuilds.net/livefeed").text
-    soup = BeautifulSoup(sourcePage, 'lxml')
+#Hit 'View More' button
+failedAttempts = 0
+for i in range(n):
+    if failedAttempts > maxFailedAttempts:
+        break
+    try:
+        load_more = driver.find_element_by_id("moreMatchesButton")
+        load_more.click()
+        time.sleep(3)
+    except:
+        time.sleep(5)
+        failedAttempts += 1
+        continue
+        
+#Get source page from the Selenium Driver
+sourcePage = driver.page_source
+soup = BeautifulSoup(sourcePage, 'lxml')
+soup = soup.find("div", class_='game-feed pro-history pos-rel')
 
-    #Get all matches from 'Live Feed' page
-    LiveFeed = soup.find('div', id="game-feed") #class_="game-feed pro-history pos-rel") #"game-feed")
-    links = soup.find_all("a")
+#Get all matches from 'Live Feed' page
+LiveFeed = soup.find('div', id="game-feed") #class_="game-feed pro-history pos-rel") #"game-feed")
+elements = soup.find_all("a")
+links = []
+for element in elements:
+    links.append("https://www.probuilds.net" + element["href"])
 
-    #List links we already have
-    linksFile = open("links.txt", "r")
-    linksInFile = linksFile.readlines()
-    linksFile.close()
+#List links we already have
+linksFile = open("links.txt", "r")
+linksInFile = linksFile.readlines()
+linksFile.close()
 
-    #Sort new matches
-    linksFile = open("links.txt", "a")
-    newLinks = []
-    for address in links:
-        if address not in linksInFile:
-            linksInFile.append(address)
-            newLinks.append(address)
+#Sort new matches
+linksFile = open("links.txt", "a")
+newLinks = []
+for address in links:
+    if address not in linksInFile:
+        linksInFile.append(address)
+        newLinks.append(address)
 
-    #Add new matches to the txt file
-    for address in newLinks:
-        linksFile.write(address+"\n")
-    linksFile.flush()
-    linksFile.close()
+#Add new matches to the txt file
+for address in newLinks:
+    linksFile.write(address+"\n")
+linksFile.flush()
+linksFile.close()
 
-    #Iteration control
-    iterationCounter +=1
-    print("Elapsed runs: ", iterationCounter)
-    print("Feel free to close this at any given time. Current iterations are set tp happen every {} hour(s)".format(interval/3600))
+driver.quit()
+print("Done fetching data")
