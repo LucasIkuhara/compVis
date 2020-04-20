e6f148cd3e2c887fc726f4734b2fefcac6954a3e
Lucas Ikuhara <lucasikuhara@poli.ufrj.br>
2020-04-20 05:31:29 -0300

    Direction vector

--- Nautilus/projector.py
+++ Nautilus/projector.py
@@ -1,8 +1,32 @@
 import cv2 
 import numpy as np
+import rospy
+from robosub_msgs.msg import Euler
+from math import sin, cos
 
 output_dimensions = [480, 720]
+rospy.init_node("cnn_prokector")
+orientation = np.array((0,0,0), dtype=np.float)
+directionVector = np.array((0,0,0), dtype=np.float)
 
+def orientationCallback(data):
+    orientation[0] = eulerNormalization(data.roll)
+    orientation[1] = eulerNormalization(data.pitch)
+    orientation[2] = eulerNormalization(data.yaw)
+    print("roll: {}, pitch: {}, yaw: {}".format(orientation[0], orientation[1], orientation[2]))
+
+    directionVector[0] = cos(orientation[2])*cos(orientation[1])
+    directionVector[1] = sin(orientation[2])*cos(orientation[1])
+    directionVector[2] = sin(orientation[1])
+    print("Directional Vector: ", directionVector)
+
+def eulerNormalization(ang):
+    if ang <= 180:
+        return ang
+    else:
+        return abs(360-ang)
+
+rospy.Subscriber("pretty/orientation", Euler, orientationCallback)
 
 while True:
     image = np.zeros(output_dimensions, dtype=np.uint8)
@@ -12,3 +36,4 @@ while True:
         break
 
 cv2.destroyAllWindows()
+rospy.spin()
